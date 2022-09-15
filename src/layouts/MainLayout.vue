9ba6fdc19dff97f1beda4cd4f31995e0b174c2c7
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-09-15 12:04:39 +0300

    feat(layouts/main): add side navigation

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,22 @@
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+      <q-item
+        v-for="link of links"
+        clickable
+        :key="link.title"
+        :to="link.to"
+        exact
+      >
+        <q-item-section avatar>
+          <q-icon :name="link.icon" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ link.title }}</q-item-label>
+          <q-item-label caption>{{ link.caption }}</q-item-label>
+        </q-item-section>
+      </q-item>
     </q-drawer>
 
     <q-page-container>
@@ -35,4 +50,23 @@ const leftDrawerOpen = ref(false);
 const toggleLeftDrawer = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 };
+
+const links = [
+  {
+    title: 'Bookmarks',
+    caption: 'Manage your bookmarks here',
+    icon: 'bookmarks',
+    to: {
+      name: 'home',
+    },
+  },
+  {
+    title: 'Trash',
+    caption: '',
+    icon: 'delete',
+    to: {
+      name: 'trash',
+    },
+  },
+];
 </script>
